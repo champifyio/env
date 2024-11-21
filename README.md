ad656a46e3f0b86e01032d4d350fd039819698e0
Dan Loewenherz <dan@lionheartsw.com>
2024-11-21 17:42:49 -0600

    update readme and API

--- README.md
+++ README.md
@@ -14,15 +14,14 @@ npm install @champify/env
 // In index.ts
 import { Env } from '@champify/env';
 
-const generateEnv = Env(['PORT', 'DATABASE_URL', 'API_KEY']);
+const e = new Env(['PORT', 'DATABASE_URL', 'API_KEY']);
 ```
 
 Then, you can use the generated context to access the environment variables.
 
 ```typescript
 // In some other file
-import { generateEnvContext } from './index';
+import { e } from './index.js';
 
-// This will throw an error if the environment variables are not set
-const env = await generateEnv(['PORT', 'DATABASE_URL', 'API_KEY']);
+const env = await e.init(['PORT', 'DATABASE_URL', 'API_KEY']);
 ```
--- src/index.ts
+++ src/index.ts
@@ -10,54 +10,57 @@ export const getNodeEnv = () => process.env.NODE_ENV || 'development';
 export const isProduction = () => getNodeEnv() === 'production';
 export const isTest = () => getNodeEnv() === 'test';
 
-export const throwIfMissing = async (key: string, value: string | null | undefined) => {
-  const NODE_ENV = getNodeEnv();
-  if (!value) {
-    if (NODE_ENV === 'production') {
+export class Env<T extends string> {
+  private throwHandler: null | ((key: string) => Promise<void>);
 
-    }
-    throw new Error(`Missing required env var ${key} (${NODE_ENV})`);
-  }
-  return value;
-};
-
-export const optional = <T extends string>(key: T, fallback = '') => ({
-  key,
-  handler: (value: string | null | undefined) => Promise.resolve(value || fallback),
-});
-
-export const optionalInDevelopment = <T extends string>(key: T, fallback = '') => ({
-  key,
-  handler: (value: string | null | undefined) =>
-    ['development', 'test', 'prodreadonly'].includes(getNodeEnv())
-      ? Promise.resolve(value || fallback)
-      : throwIfMissing(key, value),
-});
-
-export const required = <T extends string>(key: T) => ({
-  key,
-  handler: (value: string | undefined) => throwIfMissing(key, value),
-});
-
-const _generateEnvContext = async <T extends string>(
-  envVars: ReadonlyArray<T | EnvVarDefinition<T>>,
-): Promise<EnvVarContext<T>> => {
-  const context: Partial<EnvVarContext<T>> = {};
-
-  for (const envVar of envVars) {
-    if (typeof envVar === 'string') {
-      // eslint-disable-next-line no-restricted-syntax
-      context[envVar] = await throwIfMissing(envVar, process.env[envVar]);
-    } else {
-      const { key, handler } = envVar;
-      // eslint-disable-next-line no-restricted-syntax
-      context[key] = await handler(process.env[key]);
-    }
+  constructor(_: ReadonlyArray<T | EnvVarDefinition<T>>, throwHandler?: (key: string) => Promise<void> ) {
+    this.throwHandler = throwHandler ?? null;
   }
 
-  return context as EnvVarContext<T>;
-};
+  init = async (env: ReadonlyArray<T | EnvVarDefinition<T>>): Promise<EnvVarContext<T>> => {
+    const context: Partial<EnvVarContext<T>> = {};
 
-export const Env = <T extends string>(
-  definedEnvVars: ReadonlyArray<T | EnvVarDefinition<T>>,
-) => (env: ReadonlyArray<T | EnvVarDefinition<T>>) => _generateEnvContext(env);
+    for (const envVar of env) {
+      if (typeof envVar === 'string') {
+        // eslint-disable-next-line no-restricted-syntax
+        context[envVar] = await this.throwIfMissing(envVar, process.env[envVar]);
+      } else {
+        const {key, handler} = envVar;
+        // eslint-disable-next-line no-restricted-syntax
+        context[key] = await handler(process.env[key]);
+      }
+    }
+
+    return context as EnvVarContext<T>;
+  }
+
+  throwIfMissing = async (key: string, value: string | null | undefined) => {
+    const NODE_ENV = getNodeEnv();
+    if (!value) {
+      if (this.throwHandler) {
+        await this.throwHandler(key)
+      }
+      throw new Error(`Missing required env var ${key} (${NODE_ENV})`);
+    }
+    return value;
+  };
+
+  optional = <T extends string>(key: T, fallback = '') => ({
+    key,
+    handler: (value: string | null | undefined) => Promise.resolve(value || fallback),
+  });
+
+  optionalInDevelopment = <T extends string>(key: T, fallback = '') => ({
+    key,
+    handler: (value: string | null | undefined) =>
+      ['development', 'test', 'prodreadonly'].includes(getNodeEnv())
+        ? Promise.resolve(value || fallback)
+        : this.throwIfMissing(key, value),
+  });
+
+  required = <T extends string>(key: T) => ({
+    key,
+    handler: (value: string | undefined) => this.throwIfMissing(key, value),
+  });
+}
+
